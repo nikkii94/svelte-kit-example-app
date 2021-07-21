578f8da9e987dbcac7e0e6492168f805c07b03aa
Niki <k.nikolett17@gmail.com>
2021-07-21 20:54:30 +0200

    Initial commit

---
+++ src/lib/Header.svelte
@@ -0,0 +1,29 @@
+<header>
+	<h1>Welcome</h1>
+	<nav>
+		<a href='/'>Home</a>
+		<a href='/about'>About Us</a>
+	</nav>
+</header>
+
+<style lang='scss'>
+		//:global(header) {}
+    header {
+        background: rebeccapurple;
+        color: #fff;
+        padding: 20px;
+        margin: 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+		nav {
+			a {
+				letter-spacing: 1px;
+        color: #fff;
+        text-decoration: none;
+        padding: 0 10px;
+			}
+		}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from '../lib/Header.svelte';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>Made with SvelteKit 2021</footer>
+
+<style>
+	footer {
+			text-align: center;
+	}
+</style>
